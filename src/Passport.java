56d72f0ec4ab9a076ea45e77bc5b2afb2c78f0c2
Illiagitdev <illya.posv.b@gmail.com>
2019-05-10 23:29:03 +0300

    good to go, no fool check

--- src/Passport.java
+++ src/Passport.java
@@ -1,18 +1,33 @@
 import java.io.Console;
+import java.util.Scanner;
 
 public class Passport {
     public static void main(String[] args) {
 
+        //use Scanner for input
+        Scanner in = new Scanner(System.in);
+
         //person name
         String fName = null, lName = null;
+        System.out.print("Enter First Name: ");
+        fName = in.nextLine();
+        System.out.print("Enter Last name: ");
+        lName = in.nextLine();
 
         //gender & age
         String gender = null;
+        System.out.print("Enter gender: ");
+        gender = in.nextLine();
         int age = 0;
+        System.out.print("Your age: ");
+        age = in.nextInt();
 
         //person location
         String locFrom = null, locCurrent = null;
-
+        System.out.print("Place, where you were born: ");
+        locFrom = in.nextLine();
+        System.out.print("Where you live now: ");
+        locCurrent = in.nextLine();
 
         //resulted output
         System.out.flush();
