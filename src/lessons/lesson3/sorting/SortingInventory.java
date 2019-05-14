81ded5c479911340efa3cf077f213d227a66fbe5
Illiagitdev <illya.posv.b@gmail.com>
2019-05-15 00:22:52 +0300

    stuck with Swap function, how to?

--- src/lessons/lesson3/sorting/SortingInventory.java
+++ src/lessons/lesson3/sorting/SortingInventory.java
@@ -2,13 +2,47 @@ package lessons.lesson3.sorting;
 
 public class SortingInventory implements SorttingInterface
 {
-    //@java.lang.Override
-    public int[] BubbleSort(int[] numbers) {
-        return new int[0];
+    static private int countSteps = 0;
+    static private int countShifts = 0;
+    @java.lang.Override
+    public void BubbleSort(int[] numbers) {
+        int k =numbers.length;
+        for (int j = 0; j < k; j++){
+            for (int i = 0; i < k - j -2; i++){
+                if(numbers[i] < numbers[i + 1]){
+                    Swap(numbers[i], numbers[i + 1]);
+                    countShifts++;
+                }
+                countSteps++;
+            }
+        }
+       //return numbers;
     }
 
-    //@java.lang.Override
+    private void Swap(int a, int b) {
+        int z = a;
+        a = b;
+        b = z;
+    }
+
+    @java.lang.Override
     public double[] BubbleSort(double[] numbers) {
-        return new double[0];
+        int k =numbers.length;
+        for (int j = 0; j < k; j++){
+            for (int i = 0; i < k - j -2; i++){
+                if(numbers[i] < numbers[i + 1]){
+                    Swap(numbers[i], numbers[i + 1]);
+                    countShifts++;
+                }
+                countSteps++;
+            }
+        }
+        return numbers;
+    }
+
+    private void Swap(double a, double b) {
+        double z = a;
+        a = b;
+        b = z;
     }
 }
