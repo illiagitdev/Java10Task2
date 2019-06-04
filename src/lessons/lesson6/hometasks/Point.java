a62b27721c16934d0daea6f208dad8e3ce9f2c94
illia <illya.posv.b@gmail.com>
2019-06-04 18:14:34 +0300

    Point class ready

--- src/lessons/lesson6/hometasks/Point.java
+++ src/lessons/lesson6/hometasks/Point.java
@@ -13,18 +13,63 @@ public class Point {
         this.y = y;
     }
 
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    /**
+     * Calculate length between two points
+     * @param otherPoint - point to which we seek the distance
+     * @return distanse
+     */
     public double calcLength(Point otherPoint){
         double res = Math.sqrt(Math.pow(otherPoint.x - x, 2) + Math.pow(otherPoint.y - y, 2));
         return res;
     }
 
+    /**
+     * Calculate length to basis (0. 0)
+     * @return distance
+     */
     public double calcLength(){
         return calcLength(startPoint);
     };
 
+    /**
+     * Normalize vector to current Point
+     * @return - normalized vector of the point
+     */
     public Point normalize(){
-        double normalizer = calcLength(startPoint);
-        x = x / nonormalizer;
-        return new Point();
+        Point nomalizedPoint = new Point();
+        double normalizer = this.calcLength();
+        nomalizedPoint.x = x / normalizer;
+        nomalizedPoint.y = y / normalizer;
+        // checking normalized
+//        System.out.print("normalize: ");
+//        nomalizedPoint.show();
+//        double checkNormalize = Math.pow(nomalizedPoint.x, 2) + Math.pow(nomalizedPoint.y, 2);
+//        System.out.println("checkNormalize " +checkNormalize);
+        return nomalizedPoint;
     }
+
+    /**
+     * Show point - for our convenience
+     */
+    public void show(){
+        String str = "x: " + x + " y: " + y;
+        System.out.println(str);
+    }
+
+    /*public static void main(String[] args) {
+        Point test = new Point(5, 9);
+        test.show();
+
+        double z = test.calcLength();
+        System.out.println(z);
+        test.normalize();
+    }*/
 }
