016abb921b96af23ea7d81591b25507c73806fe8
illiagitdev <illya.posv.b@gmail.com>
2019-08-05 18:11:29 +0300

    lesson8_1 7:40

---
+++ src/lessons/lesson8_1/ThreadPoolExample.java
@@ -0,0 +1,19 @@
+package lessons.lesson8_1;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolExample {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 20; i++) {
+            service.submit(() -> {
+                System.out.println("Hello from thread pool :D");
+            });
+        }
+
+        service.shutdown();
+    }
+}
